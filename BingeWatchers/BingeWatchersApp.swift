f64491b5294177a8a15c2bd0458bc1226505a50b
ajandaur <anmol.jandaur@gmail.com>
2022-01-10 10:43:08 -0800

    Initial Commit

---
+++ BingeWatchers/BingeWatchersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingeWatchersApp.swift
+//  BingeWatchers
+//
+//  Created by Anmol  Jandaur on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BingeWatchersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
